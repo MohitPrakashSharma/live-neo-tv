b315acbbc7ec7a5f250a25116e1b7ad1214f71c9
Mohit Sharma <mohi@mediahubster.com>
2024-12-27 22:27:13 +0530

    working on the post

--- src/components/Skeletons/HeaderSkeleton.tsx
+++ src/components/Skeletons/HeaderSkeleton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const HeaderSkeleton = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 bg-[#262626] text-white z-40">
+    <header className="fixed top-0 left-0 right-0 bg-black text-white z-40">
       <div className="container-fluid mx-auto px-4 h-16 flex items-center justify-between animate-pulse">
         {/* Logo Placeholder */}
         <div className="flex items-center gap-4">
