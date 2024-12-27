ce17c782c9671c140e1142dbbc5b58a61d76f538
Mohit Sharma <mohi@mediahubster.com>
2024-12-27 20:05:30 +0530

    working on the Skeleton

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { VideoPlayer } from '../components/VideoPlayer/VideoPlayer';
 import { PopularChannelsGrid } from '../components/PopularChannels/PopularChannelsGrid';
@@ -10,6 +10,23 @@ export const HomePage = () => {
   const navigate = useNavigate();
   const { selectedChannel, setSelectedChannel } = useSelectedChannel();
   const [selectedCategory, setSelectedCategory] = useState('');
+  const [paddingTop, setPaddingTop] = useState('11rem');
+
+
+  useEffect(() => {
+    const updatePadding = () => {
+      if (window.innerWidth >= 768) {
+        setPaddingTop('23rem'); // Tablet and desktop
+      } else {
+        setPaddingTop('11rem'); // Mobile
+      }
+    };
+
+    updatePadding(); // Set initial value
+    window.addEventListener('resize', updatePadding);
+
+    return () => window.removeEventListener('resize', updatePadding);
+  }, []);
 
   const handleChannelSelect = (id: string, channelName: string) => {
     setSelectedChannel(prevChannel => {
@@ -28,9 +45,9 @@ export const HomePage = () => {
     <main>
       <VideoPlayer channel={selectedChannel} />
       
-      <div className="mt-[300px] md:mt-[380px] lg:mt-[440px]">
-        <div className="mt-8 md:mt-12 pt-16 md:pt-20 mb-8">
-          <PopularChannelsGrid onChannelSelect={handleChannelSelect} />
+      <div className="mt-[180px] md:mt-[200px] lg:mt-[220px]">
+        <div className="mt-8 md:mt-12 pt-16 md:pt-20 mb-8"  style={{ paddingTop }}>
+          <PopularChannelsGrid onChannelSelect={handleChannelSelect}/>
         </div>
         
         <ProgramGuide
